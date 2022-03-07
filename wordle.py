2c360c2586e67ebf96c80d28afd15f7b20bd81a6
Luis Belloch <luis.bellochmartinez@gmail.com>
2022-03-07 11:55:51 +0100

    checkpoint 3

--- wordle.py
+++ wordle.py
@@ -1,6 +1,7 @@
 import math
 from operator import add, contains
 from random import random
+from select import select
 
 def choose_secret(filename):
     """Dado un nombre de fichero, esta funcionn devuelve una palabra aleatoria de este fichero transformada a mayÃºsculas.
@@ -63,8 +64,12 @@ def print_word(word, same_letter_position, same_letter):
         transformed[i] = word[i].lower()
     for i in same_position:
         transformed[i] = word[i].upper()
-    return str(transformed)
-def choose_secret_advanced():
+    # String
+    result = ""
+    for l in transformed:
+        result += l
+    return result
+def choose_secret_advanced(filename):
     """Dado un nombre de fichero, esta funciÃ³n filtra solo las palabras de 5 letras que no tienen acentos (Ã¡,Ã©,Ã­,Ã³,Ãº). De estas palabras, la funciÃ³n devuelve una lista de 15 aleatorias no repetidas y una de estas 15, se selecciona aleatoriamente como palabra secret.
     Args:
       filename: El nombre del fichero. Ej. "palabras_extended.txt"
@@ -72,7 +77,40 @@ def choose_secret_advanced():
       selected: Lista de 15 palabras aleatorias no repetidas que tienen 5 letras y no tienen acentos
       secret: Palabra elegida aleatoriamente de la lista de 15 seleccionadas transformada a mayÃºsculas
     """
- 
+    f = open(filename + ".txt", mode="r", encoding="utf-8")
+
+    archivo = ""
+    texto = f.readline()
+    archivo += texto;
+    while texto != "":
+        texto = f.readline()
+        archivo += texto;
+    f.close();
+    palabras = archivo.split()
+
+    selected = []
+    filtradas = []
+    # Se filtran
+    for i in range(len(palabras)):
+        # Sin acentos
+        if(palabras[i].isascii()):
+            # De 5 letras
+            if(len(palabras[i]) == 5):
+                filtradas.append(palabras[i])
+    # Si hay menos de 15 palabras el archivo no es valido
+    if 
+    # Elige 15 palabras al azar, puede hacerse de otra forma pero esta es rapida de escribir
+    while len(selected) < 15:
+        rand = math.floor(random() * len(filtradas))
+        # No esta en la lista
+        if not (contains(selected, palabras[rand])):
+            selected.append(palabras[rand].upper())
+
+    rand = math.floor(random() * len(selected))
+    secret = selected[rand]
+    return selected, secret
+
+
 def check_valid_word():
     """Dada una lista de palabras, esta funciÃ³n pregunta al usuario que introduzca una palabra hasta que introduzca una que estÃ© en la lista. Esta palabra es la que devolverÃ¡ la funciÃ³n.
     Args:
@@ -80,19 +118,20 @@ def check_valid_word():
     Returns:
       word: Palabra introducida por el usuario que estÃ¡ en la lista.
     """
-
-print("Archivo de palabras: ")
-archivo = input()
+    
 
 if __name__ == "__main__":
-    secret=choose_secret(archivo)
+    archivo = input("Archivo de palabras: ")
+    selected, secret=choose_secret_advanced(archivo)
+    print("Lista de palabras seleccionadas: ")
+    print(selected) #Debug: para ver que selecciona solo palabras sin acentos
     print("Palabra a adivinar: "+secret)#Debug: esto es para que sepas la palabra que debes adivinar
     for repeticiones in range(0,6):
         word = input("Introduce una nueva palabra: ")
         same_position, same_letter = compare_words(word, secret)
         resultado=print_word(word, same_position, same_letter)
         print(resultado)
-        if word == secret:
+        if resultado == secret:
             print("HAS GANADO!!")
             exit()
     print("LO SIENTO, NO LA HAS ADIVINIDADO. LA PALABRA ERA "+secret)   
